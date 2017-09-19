b5b27c33135d916a8acfc11dfd45f635b1603f36
apple2@ioioinfo.com <apple2@ioioinfo.com>
2017-09-19 20:53:00 +0800

    update

--- server/socket/socket_io.js
+++ server/socket/socket_io.js
@@ -2,6 +2,7 @@ const uu_request = require('../utils/uu_request');
 const uuidV1 = require('uuid/v1');
 var eventproxy = require('eventproxy');
 var service_info = "vehicles GPS service";
+var socket_map = {};
 
 exports.register = function(server, options, next){
 
@@ -10,8 +11,15 @@ exports.register = function(server, options, next){
     io.on('connection', function (socket) {
         console.log('New connection!');
         socket.emit('hello', 'can you hear me?', 1, 2, 'abc');
+
+        socket.on('cookie', function(msg){
+
+          socket_map[msg] = socket.id;
+          io.emit('chat message', msg);
+        });
         socket.on('chat message', function(msg){
-          console.log('message: ' + msg);
+          console.log('io.sockets:'+io.sockets);
+
           io.emit('chat message', msg);
         });
 
@@ -26,16 +34,18 @@ exports.register = function(server, options, next){
             method: 'GET',
             path: '/socket',
             handler: function(request, reply){
-                reply.view('index');
+                var cookie = uuidV1();
+                reply.view('index',{"cookie":JSON.stringify(cookie)});
             }
         },
         {
             method: 'GET',
             path: '/receive',
             handler: function(request, reply){
+                var cookie = request.query.cookie;
                 var msg = request.query.msg;
                 console.log(msg);
-                io.emit('chat message', msg);
+                io.sockets.connected[socket_map[cookie]].emit('chat message', msg);
                 return reply({"success":true});
             }
         },
